a6251789978ecdbb58196581c328883010c61e1c
Rachel Miles <milesrachel9@gmail.com>
2018-03-24 13:34:25 -0600

    add simple 404

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Image, Segment} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class NoMatch extends Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>
-        Page Not Found
-        <Link to='/'> Home</Link>
-      </Header>
+      <div>
+        <Header as='h1' textAlign='center'>
+          Sorry, we can not find your page! Here is a link back
+          <Link to='/'> Home</Link>
+        </Header>
+        <Segment>
+          <Image src="https://s3.envato.com/files/227378456/590.jpg" huge centered/>
+        </Segment>
+      </div>
     );
   }
 }
