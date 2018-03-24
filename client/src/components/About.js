d52c908a114ec9c6262bb194b9f2c44d25359374
Rachel Miles <milesrachel9@gmail.com>
2018-03-24 12:08:01 -0600

    about page content, styling unfinished

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -15,11 +15,31 @@ class About extends Component {
         <br />
         <Container>
           <Segment>
-            <Header as='h3' textAlign='center'>
-            We're a small Chinese restaurant with over 20 years of experience between food and service. We're confident you'll love our food, so come in and try it! Can't come in? We deliver and cater!
+            <Header as='h4' textAlign='center'>
+            We're a small Chinese restaurant with over 20 years of experience between food and service. We're confident you'll love our food, so come in and try it! 
+            <br />
+            <br/>
+            Can't come in? We deliver and cater!
             </Header>
-            <Segment>
-            </Segment>
+            <Header as='h3'>Business Hours</Header>
+            Open Monday to Saturday, 11 AM to 10 PM.
+            <br />
+            Closed Sundays.
+            <Header as='h3' >Location </Header>
+            1331 E 3900 S Salt Lake City, Utah 84124
+              <Segment>
+                <Embed
+                  aspectRatio='21:9'
+                  autoplay = {true}
+                  url="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6975.304065536179!2d-111.8551725151177!3d40.68668399458576!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x875261ded0bb195b%3A0x20ea642e49e8b9a2!2sDragon+Diner!5e0!3m2!1sen!2sus!4v1521912328908"
+                />
+              </Segment>
+            <Header as='h3' >Contact Information</Header>
+            801.272.9333 (phone)
+            <br />
+            801.272.0470 (fax)
+            <br />
+            dragondiner@gmail.com
           </Segment>
         </Container>
       </div>
